9c31c32d13f3208f57dd8c34b387fa54ce86b05d
SahanaAvantika <sahanaavantika@gmail.com>
2025-02-01 00:19:13 +0800

    add JUnit test

---
+++ src/test/java/Task/ToDosTest.java
@@ -0,0 +1,15 @@
+package Task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDosTest {
+
+    @Test
+    public void testToString() {
+        ToDos todo = new ToDos("Read a book");
+        assertEquals("[T][ ] Read a book", todo.toString(), "toString() does not match expected format.");
+    }
+
+}
