5c05f48f2ab240a7a43a269f4ee684ee97e65222
SahanaAvantika <sahanaavantika@gmail.com>
2025-02-02 23:36:51 +0800

    add Level 9

--- src/main/java/Task/Task.java
+++ src/main/java/Task/Task.java
@@ -9,18 +9,32 @@ public abstract class Task {
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as completed.
+     */
     public void markAsDone(){
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as incomplete.
+     */
     public void markAsIncomplete(){
         this.isDone = false;
     }
 
+    /**
+     * Returns a string of the task description
+     */
     public String getDescription(){
         return description;
     }
 
+    /**
+     * Returns a string representation of the task, including its completion status.
+     *
+     * @return A string indicating whether the task is done ("[X]") or not ("[ ]"), followed by the task description.
+     */
     @Override
     public String toString(){
         String s = isDone ? "[X]" : "[ ]";
